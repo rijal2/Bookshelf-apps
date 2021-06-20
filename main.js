ba1ff6bb4ce5e5c60fe61c5b338cc10dfa08267f
rijal2 <merentas5benua27@gmail.com>
2021-06-20 14:12:11 +0700

    01. Membuat buku tersimpan di DOM bag.1

---
+++ main.js
@@ -0,0 +1,11 @@
+// membuat form submit bekerja dengan baik
+const inputBook = document.querySelector("#inputBook");
+
+inputBook.addEventListener("submit", simpan);
+
+function simpan(e){
+    e.preventDefault();
+
+    
+}
+
